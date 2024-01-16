4ebf74264991e5c0e9ea831d71bb9ad69725af9a
Ian Irving <ian@falsepositives.com>
2024-01-16 18:02:37 -0500

    Add getCurrentTime function and test

---
+++ test/time.test.js
@@ -0,0 +1,12 @@
+import { test, expect, vi } from "vitest";
+import { getCurrentTime } from "../src/time.js";
+
+test("time", () => {
+  vi.setSystemTime(new Date("2021-06-29T17:52:11.000Z"));
+
+  const time = getCurrentTime();
+
+  expect(time).toBe(`13:52:11`);
+
+  vi.useRealTimers();
+});
