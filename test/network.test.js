16700d012e6d5a7b263d620f103ca7087a330bec
Ian Irving <ian@falsepositives.com>
2024-01-17 12:39:27 -0500

    Add network test with msw http mock

--- test/network.test.js
+++ test/network.test.js
@@ -1,32 +1,24 @@
-import { test, expect, vi } from "vitest";
+import { test, expect, vi, beforeAll, afterEach } from "vitest";
 import { getPostBodybyId } from "../src/network.js";
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
 
-vi.stubGlobal("fetch", async (url) => {
-  return {
-    json: async () => {
-      return {
-        id: 1,
-        title:
-          "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-        userId: 1,
-      };
-    },
-  };
-});
+const server = setupServer(
+  http.get("https://jsonplaceholder.typicode.com/posts/1", () => {
+    return HttpResponse.json({
+      body: "Mocked Body",
+    });
+  })
+);
 
-test("greeting", async () => {
+beforeAll(() => server.listen());
+afterEach(() => server.close());
+
+test("network getPostBodybyId should fetch", async () => {
   const result = await getPostBodybyId(1);
 
-  expect(result).toMatchInlineSnapshot(`
-    {
-      "body": "quia et suscipit
-    suscipit recusandae consequuntur expedita et cum
-    reprehenderit molestiae ut ut quas totam
-    nostrum rerum est autem sunt rem eveniet architecto",
-      "id": 1,
-      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      "userId": 1,
+  expect(result).toMatchInlineSnapshot(`{
+      "body": "Mocked Body",
     }
   `);
 });
