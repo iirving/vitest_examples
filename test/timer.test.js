4aa4596f198abcd825d1b9b73af1218afa49f9ff
Ian Irving <ian@falsepositives.com>
2024-01-17 11:44:17 -0500

    Add timer test cases and beforeEach setup

--- test/timer.test.js
+++ test/timer.test.js
@@ -1,14 +1,67 @@
-import { warnLater } from "../src/timer.js";
-import { test, expect, vi } from "vitest";
+/**
+ * @fileoverview This file contains test cases for the timer module.
+ * @module timer.test
+ */
 
-test("timer warnLater", async () => {
+import { warnLater } from "../src/timer.js";
+import { test, expect, vi, beforeEach } from "vitest";
+
+/**
+ * Sets up the fake timers before each test case.
+ */
+beforeEach(() => {
+  vi.useFakeTimers();
+});
+
+/**
+ * Skipped test case for the timer warnLater function with long duration.
+ * the promise wrapped setTime will take 2 seconds and cuase the the test suite to fail
+ * this test case is skipped to avoid the failure
+ * keep here only as a example of the simple simplest way to pass the test, in theory
+ */
+// test.skip("timer warnLater incremental with setTimeout WARNING long duration ", async () => {
+//   const sampleString = "Hello world!";
+//   const logSpy = vi.spyOn(console, "log");
+
+//   warnLater(sampleString);
+//   expect(logSpy).not.toHaveBeenCalled();
+
+//   await new Promise((resolve) => setTimeout(resolve, 2_000));
+
+//   expect(logSpy).toHaveBeenCalledWith(sampleString);
+// });
+
+/**
+ * Test case for the timer warnLater function with incremental time.
+ * It checks if the log message is not called immediately and is called after a specific time.
+ */
+test("timer warnLater incremental ", async () => {
+  const sampleString = "Hello world!";
+
   const logSpy = vi.spyOn(console, "log");
 
-  warnLater("Hello, world!");
-
+  warnLater(sampleString);
   expect(logSpy).not.toHaveBeenCalled();
 
-  await new Promise((resolve) => setTimeout(resolve, 2_000));
+  vi.advanceTimersByTime(1999);
+  expect(logSpy).not.toHaveBeenCalled();
 
-  expect(logSpy).toHaveBeenCalledWith("Hello, world!");
+  vi.advanceTimersByTime(1);
+  expect(logSpy).toHaveBeenCalledWith(sampleString);
+});
+
+/**
+ * Test case for the timer warnLater function.
+ * It checks if the log message is not called immediately and is called when the next timer is reached.
+ */
+test("timer warnLater", async () => {
+  const sampleString = "Hello world!";
+
+  const logSpy = vi.spyOn(console, "log");
+
+  warnLater(sampleString);
+  expect(logSpy).not.toHaveBeenCalled();
+
+  vi.advanceTimersToNextTimer();
+  expect(logSpy).toHaveBeenCalledWith(sampleString);
 });
