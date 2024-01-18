1db8054192737dff7e1e044091f612f9b7b88fb7
Ian Irving <ian@falsepositives.com>
2024-01-18 10:32:18 -0500

    Update vite.config.ts with test setup files and alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vitest/config";
+// import { resolve } from "node:path";
+import { resolve } from "path";
+import path from "path";
 
 export default defineConfig({
   test: {
     setupFiles: ["test/msw.setup.ts"],
+    alias: {
+      "@/": new URL("./src/", import.meta.url).pathname,
+    },
   },
 });
