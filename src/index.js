a4697058eb45bb51f36e886e0bb277c89fdae460
Ian Irving <ian@falsepositives.com>
2024-02-26 17:55:52 -0500

    Add deepMerge function to merge arrays and objects recursively

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 export function deepMerge(a, b) {
+  // If both are arrays, merge them
   if (Array.isArray(a) && Array.isArray(b)) {
     return [...a, ...b];
   }
 
+  // If both are of different types, throw an error
   if (Array.isArray(a) || Array.isArray(b) || typeof a !== typeof b) {
     throw new Error("Error: Cannot merge two differnet types.");
   }
 
+  // recorively merge the objects
   const merged = { ...a };
   for (const key of Object.keys(b)) {
     if (typeof b[key] === "object" || Array.isArray(a[key])) {
